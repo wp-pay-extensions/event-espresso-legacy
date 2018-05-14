65843cac684f245550aed7ad462f483cb57b34e5
Remco Tolsma <info@remcotolsma.nl>
2018-05-14 10:10:34 +0200

    Getting ready for version 2.0.0.

--- tests/EventEspressoTest.php
+++ tests/EventEspressoTest.php
@@ -11,14 +11,14 @@ use PHPUnit_Framework_TestCase;
  * Company: Pronamic
  *
  * @author  Reüel van der Steege
- * @version unreleased
- * @since   unreleased
+ * @version 2.0.0
+ * @since   2.0.0
  */
 class EventEspressoTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * Test class.
 	 */
 	public function test_class() {
-		$this->assertTrue( class_exists( 'Pronamic\WordPress\Pay\Extensions\EventEspressoLegacy\EventEspresso' ) );
+		$this->assertTrue( class_exists( __NAMESPACE__ . '\EventEspresso' ) );
 	}
 }
